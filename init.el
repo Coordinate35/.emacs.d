f63d23a502051f35d36fb82e7d07322256c92a85
weijunjie <weijunjie@didiglobal.com>
2023-02-09 21:03:07 +0800

    add go test

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
+   '(gotest molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
