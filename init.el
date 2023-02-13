5e80bc4844bff06756bdd8c065c2a1cc17b376e2
weijunjie <weijunjie@didiglobal.com>
2023-02-13 16:25:30 +0800

    multiple cursor support

--- init.el
+++ init.el
@@ -15,6 +15,8 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'molokai t)
 
+(require 'multiple-cursors)
+
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 
@@ -35,7 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(gotest molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
+   '(multiple-cursors gotest molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
