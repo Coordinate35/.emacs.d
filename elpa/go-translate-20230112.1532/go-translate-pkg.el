718f53e9ff334b75e8d803f8811244c766df3157
weijunjie <weijunjie@didiglobal.com>
2023-02-22 20:49:59 +0800

    go-traslate and rfc-mode

---
+++ elpa/go-translate-20230112.1532/go-translate-pkg.el
@@ -0,0 +1,12 @@
+(define-package "go-translate" "20230112.1532" "Translation framework supports multiple engines such as Google/Bing/DeepL"
+  '((emacs "27.1"))
+  :commit "e8343e7d41af67f55c2da9231fb275a93382a4c8" :authors
+  '(("lorniu" . "lorniu@gmail.com"))
+  :maintainer
+  '("lorniu" . "lorniu@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/lorniu/go-translate")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
