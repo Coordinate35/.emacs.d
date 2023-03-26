db55fb28536476954b6e688172677dffc9dbc07f
Coordinate35 <Coordinate35@hust.edu.cn>
2023-03-26 20:43:02 +0800

    update org mode

--- init.el
+++ init.el
@@ -45,6 +45,9 @@
 (require 'org-onenote)
 
 (setq org-log-done 'time)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
 
 (require 'elisp-format)
 
