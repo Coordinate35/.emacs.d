d8f8548e040792b5ae5dcae5852fac03c58872f4
Coordinate35 <Coordinate35@bingyan.net>
2023-06-01 02:14:23 +0800

    c

--- init.el
+++ init.el
@@ -79,7 +79,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dap-mode elisp-format impatient-mode go-translate rfc-mode window-numbering multiple-cursors gotest molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
+   '(clang-format dap-mode elisp-format impatient-mode go-translate rfc-mode window-numbering multiple-cursors gotest molokai-theme neotree company-go company auto-complete lsp-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -92,7 +92,9 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c-mode-hook (lambda ()
 			 (setq tab-width 4)
-			 (require 'dap-cpptools)))
+			 (require 'dap-cpptools)
+			 (require 'clang-format)
+			 (setq clang-format-style "file")))
 (add-hook 'c++-mode-hook 'lsp)
 
 
